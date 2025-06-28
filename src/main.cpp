51ad8663d820b77ebfe79570600466a4ac55192e
Aayush Acharya <aayushacharya007@gmail.com>
2025-06-28 12:26:10 -0500

    added type builtin and refactored the code logic

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,15 @@
 
 using namespace std;
 
+string extractCommand(const string& input) {
+  int start = 0;
+  int end = input.find(' ');
+  int sizeCmd = end - start;
+  string command = input.substr(start, sizeCmd);
+
+  return command;
+}
+
 void echo(string& input) {
   int start = input.find(' ') + 1;
   int end = input.length();
@@ -11,6 +20,39 @@ void echo(string& input) {
   string echoStr = input.substr(start, sizeOfEchoStr);
 
   cout << echoStr << endl;
+}
+
+void type(string& input) {
+  int start = input.find(' ') + 1;
+  int end = input.length();
+  int sizeOfTypeStr = end - start;
+
+  string typeStr = input.substr(start, sizeOfTypeStr);
+  string outputText = "";
+
+  enum Types {echo, exit, type, invalid};
+  Types Type = invalid;
+
+  if (typeStr == "echo") Type = echo;
+  else if (typeStr == "exit") Type = exit;
+  else if (typeStr == "type") Type = type;
+
+  switch (Type) {
+    case echo:
+      outputText = "echo is a shell builtin";
+      break;
+    case exit:
+      outputText = "exit is a shell builtin";
+      break;
+    case type:
+      outputText = "type is a shell builtin";
+      break;
+    default:
+      outputText = typeStr + ": not found";
+      break;
+  }
+
+  cout << outputText << endl;
 }
 
 void repl(string& input) {
@@ -27,9 +69,16 @@ void repl(string& input) {
         return;
       }
 
-      // check for echo
-      if (input.find("echo") != string::npos) {
+      // extract the command
+      string command = extractCommand(input);
+
+      // check for various commands
+      if (command == "echo") {
         echo(input);
+        continue;
+      }
+      else if (command == "type") {
+        type(input);
         continue;
       }
 
